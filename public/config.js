843a3976934250831a3d50b91c7aa82d9e5e0975
chenxiaofei <481617494@qq.com>
2022-12-11 16:02:40 +0800

    first commit

---
+++ public/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '亦失 - 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u1921144-2ee080a97efb592340f2d6d6', // 总 Api key
+    // 'm793114089-3d5eaad446f0188dac5e5568', // 亦失小栈 - 首页
+    // 'm793114141-c7a3ba51c801bfd1f294d6c3', // 亦失小栈 - 搜索引擎
+    // 'm793114145-bd34a9bd7bf5ba67a08b17d3', // 亦失小栈 - 网址集
+    // 'm793114153-6d45a0c2985017d7122b66cb'  // 亦失小栈 - 音乐播放器
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://www.cxfyyds.xyz/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.cxfyyds.xyz/'
+    }
+  ],
+};
